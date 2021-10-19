cfceec02926dd32425c650c51a6b9ebeaa38066f
praveenkalep <kalep20096@gmail.com>
2021-10-19 22:17:42 +0530

    edit

--- src/components/application/application.js
+++ src/components/application/application.js
@@ -7,38 +7,42 @@ import "./application.css";
             <div className="application-form">
               <h2 className="application-title">Application</h2>
               <form className="application-register" id="register-form">
-                <div className="form-group">
-                  <label htmlFor="fname">
+                <div className="application-group">
+                  <label htmlFor="name">
                   <i class="zmdi zmdi-account material-icons-name"></i></label>
                     <input type="text" id="name" name="name" autoComplete="off" placeholder=" User Name"/>
                 </div>
                 <br/>
-                               
-                <div className="form-group">
+                <div className="application-group">
+                  <label htmlFor="dob">
+                  <i class="zmdi zmdi-calendar-check material-icons-calender-check"></i></label>
+                    <input type="text" id="dob" name="dob" autoComplete="off" placeholder="Date Of Birth"/>
+                </div> 
+                <br/>         
+                <div className="application-group">
                   <label htmlFor="phoneno">
                   <i class="zmdi zmdi-phone-in-talk material-icons-phone-in-talk"></i></label>
                     <input type="text" id="phoneno" name="phoneno" autoComplete="off" placeholder=" Phone Number"/>
                 </div>
                 <br/>
-                <div className="form-group">
+                <div className="application-group">
                   <label htmlFor="email">
                   <i class="zmdi zmdi-email material-icons-email"></i></label>
-                    <input type="text" id="email" name="email" autoComplete="off" placeholder=" E-mail"/>
+                    <input type="email" id="email" name="email" autoComplete="off" placeholder=" E-mail"/>
                 </div>
                 <br/>
-                <div className="form-group">
-                  <label htmlFor="password">
-                  <i class="zmdi zmdi-lock material-icons-lock"></i></label>
-                    <input type="text" id="password" name="password" autoComplete="off" placeholder=" Password"/>
+                <div className="application-group">
+                  <label htmlFor="cv" className="bd">
+                  <i class="zmdi zmdi-attachment material-icons-attachment"></i>  Attached your certificate</label>
+                    <input type="file" id="ceno" name="ceno" autoComplete="off" placeholder="Attached your certificate"/>
+                </div>                         
+               <br/>     
+               <div className="application-group">
+                  <label htmlFor="cv" className="bd">
+                  <i class="zmdi zmdi-attachment material-icons-attachment"></i>  Attached your biodata </label>
+                    <input type="file" id="cv" name="cv" autoComplete="off"/>
                 </div>
                 <br/>
-                <div className="form-group">
-                  <label htmlFor="cpassword">
-                  <i class="zmdi zmdi-lock material-icons-lock"></i></label>
-                    <input type="text" id="cpassword" name="cpassword" autoComplete="off" placeholder=" Confirm Password"/>
-                </div> 
-                <br/>     
-
                 <div className="buton">
                   <input className="button" value="Register" type="button"/>
                   </div>    
